6e0d03092a267bd4ccd89f76f4ee7693159ae316
Dragosh Iarovoi <Dragosh Iarovoi>
2016-12-07 14:00:49 -0700

    Moved common Cored to windows

--- windows_CORE/main.c
+++ windows_CORE/main.c
@@ -1,6 +1,16 @@
-#include <windows.h>
+#include "1_includes.c"
 
-int main(int argc, char* argv)
+#include "2_global_variables.c"
+
+#include "3_operation_codes.c"
+
+#include "4_editor.c"
+
+int main (int argc, char* argv)
 {
-  
+  #include "5___setup.c"
+
+  #include "6___editor_loop.c"
+
+  #include "7___cleanup.c"
 }
