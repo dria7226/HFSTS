618e2a5bfad1bc5c49ae6887a3703c6db0b91b85
CptClutch <dria7226@colorado.edu>
2017-01-04 00:50:01 -0700

    Minor additions.

--- windows_interpreter/test.c
+++ windows_interpreter/test.c
@@ -5,6 +5,45 @@
 DATA_TYPE loop = 0;
 
 ////////////////////////////////////////////////////////
+printf("Checking memory allocation\n\n");
+
+printf("Showing program\n");
+printf("program_capacity: %u\n",program_capacity);
+
+if( program == NULL ) printf("ERROR: no memory allocated for 'program'.\n");
+ENTER_TO_CONTINUE
+
+for(; loop < program_capacity*DATA_AT(SETTINGS, PROGRAM_CHUNK_SIZE); loop++)
+{
+  if( program[loop] == NULL) printf("ERROR: no memory allocated for 'program[%u]'.\n", loop);
+else
+  printf("program[%u][%u] = %u \n",loop/DATA_AT(SETTINGS, PROGRAM_CHUNK_SIZE),loop % DATA_AT(SETTINGS, PROGRAM_CHUNK_SIZE),PROGRAM_AT(loop));
+}
+ENTER_TO_CONTINUE
+
+printf("Showing data\n");
+printf("data_capacity: %u\n",data_capacity);
+
+if( data == NULL ) printf("ERROR: no memory allocated for 'data'.\n");
+ENTER_TO_CONTINUE
+
+loop = 0;
+for(; loop < data_capacity; loop++)
+{
+
+  if( data[loop] == NULL )
+    printf("ERROR: no memory allocated for 'data[%u]'.\n", loop);
+  else
+  {
+  DATA_TYPE inner_loop = 0;
+  for(; inner_loop < data[loop].capacity; inner_loop++)
+  printf("data[%u].data[%u] = %u \n",loop,inner_loop,data[loop].data[inner_loop]);
+  ENTER_TO_CONTINUE
+  }
+}
+ENTER_TO_CONTINUE
+
+////////////////////////////////////////////////////////
 printf("Checking preprocessor definitions\n\n");
 
 printf("AT_HEAD_OFFSET(0): %u\n",AT_HEAD_OFFSET(0));
@@ -13,33 +52,6 @@ printf("DATA_AT(0,0): %u\n\n",DATA_AT(0,0));
 printf("FLAG_AT(OVERFLOW): %u\n",FLAG_AT(OVERFLOW));
 printf("FLAG_AT(UNDERFLOW): %u\n",FLAG_AT(UNDERFLOW));
 printf("FLAG_AT(PROGRAM_RESIZE_FAILED): %u\n",FLAG_AT(PROGRAM_RESIZE_FAILED));
-ENTER_TO_CONTINUE
-
-////////////////////////////////////////////////////////
-printf("Checking memory allocation\n\n");
-
-printf("Showing program\n");
-printf("program_capacity: %u\n",program_capacity);
-ENTER_TO_CONTINUE
-
-for(; loop < program_capacity*DATA_AT(SETTINGS, PROGRAM_CHUNK_SIZE); loop++)
-{
-  printf("program[%u][%u] = %u \n",loop/DATA_AT(SETTINGS, PROGRAM_CHUNK_SIZE),loop % DATA_AT(SETTINGS, PROGRAM_CHUNK_SIZE),PROGRAM_AT(loop));
-}
-ENTER_TO_CONTINUE
-
-printf("Showing data\n");
-printf("data_capacity: %u\n",data_capacity);
-ENTER_TO_CONTINUE
-
-loop = 0;
-for(; loop < data_capacity; loop++)
-{
-  DATA_TYPE inner_loop = 0;
-  for(; inner_loop < data[loop].capacity; inner_loop++)
-  printf("data[%u].data[%u] = %u \n",loop,inner_loop,data[loop].data[inner_loop]);
-  ENTER_TO_CONTINUE
-}
 ENTER_TO_CONTINUE
 
 //check core programs
