41b4ce271585e2d70ed797bb0f7dc18cef03cb23
CptClutch <dria7226@colorado.edu>
2017-01-14 15:41:44 -0700

    Started error flag work

--- windows_interpreter/SET_DESTINATION.c
+++ windows_interpreter/SET_DESTINATION.c
@@ -1,5 +1,8 @@
 // SET_DESTINATION , address1
-destination_array = AT_HEAD_OFFSET(1);
+if(data[AT_HEAD_OFFSET(1)] < data_capacity)
+  destination_array = AT_HEAD_OFFSET(1);
+else
+  //FLAG_AT() = 1;
 
 #ifdef TESTING
   PRINT("SET_DESTINATION, %u\n", AT_HEAD_OFFSET(1),0,0);
