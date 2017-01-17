14e0e889d8ed26fa6e503f831f79cabe8ea3668f
root <dria7226@gmail.com>
2017-01-17 20:59:16 +0000

    Working on bugs in linux interpreter

--- common_interpreter/test.c
+++ common_interpreter/test.c
@@ -1,7 +1,7 @@
 #ifdef TESTING_CLI
 #define ENTER_TO_CONTINUE printf("Press Enter to continue...\n");while(getchar() != '\n'); no_of_messages = 0;
 #define SCREEN_LIMIT 20
-#define PRINT(x,y,z,w) printf(x,y,z); no_of_messages++;
+#define PRINT(x,y,z,w) printf(x,y,z,w); no_of_messages++;
 #define CHECK_FOR_LIMIT if(no_of_messages > SCREEN_LIMIT) { ENTER_TO_CONTINUE }
 #define CHECK_FOR_SKIP(message, code) printf("Skip %s?(y/n)\n", message); if(getchar() == 'n' && getchar() == '\n'){code}
 
--- linux_interpreter/2_2_memory.c
+++ linux_interpreter/2_2_memory.c
@@ -3,34 +3,82 @@
 void allocate_memory()
 {
   program_capacity = DEFAULT_SIZE;
-
+  
   data_capacity = DEFAULT_SIZE*(DEFAULT_SIZE > NUMBER_OF_DEFAULT_ARRAYS) + NUMBER_OF_DEFAULT_ARRAYS*(DEFAULT_SIZE < NUMBER_OF_DEFAULT_ARRAYS);
 
   //DATA
   data = (struct ARRAY*) mmap(NULL, data_capacity * sizeof(struct ARRAY), PROT_READ|PROT_WRITE, MAP_ANONYMOUS, -1 ,0);
 
+  if(data == NULL)
+  {
+    #ifdef TESTING_CLI
+    PRINT("Couldn't allocate memory to 'data'.\n",0,0,0)
+    #endif
+    return 0;
+  }
+  
   //HEADS
   data[HEADS].capacity = 1;
   data[HEADS].data = (DATA_TYPE*)mmap(NULL, data[HEADS].capacity * sizeof(DATA_TYPE), PROT_READ|PROT_WRITE, MAP_ANONYMOUS, -1 ,0);
 
+  if(data[HEADS].data == NULL)
+  {
+    #ifdef TESTING_CLI
+    PRINT("Couldn't allocate memory for 'data[HEADS].data'.\n",0,0,0)
+    #endif
+    return 0;
+  }
+  
   // SETTINGS array
   data[SETTINGS].capacity = NUMBER_OF_SETTINGS;
   data[SETTINGS].data = load_settings();
+
+  if(data[SETTINGS].data == NULL)
+  {
+    #ifdef TESTING_CLI
+    PRINT("Couldn't allocate memory for 'data[SETTINGS].data\n'",0,0,0)
+    #endif
+    return 0;
+  }
 
   // FLAGS array
   data[FLAGS].capacity = NUMBER_OF_FLAGS;
   data[FLAGS].data = (DATA_TYPE*)mmap(NULL, data[FLAGS].capacity * sizeof(DATA_TYPE), PROT_READ|PROT_WRITE, MAP_ANONYMOUS, -1 ,0);
 
+  if(data[FLAGS].data == NULL)
+  {
+    #ifdef TESTING_CLI
+    PRINT("Couldn't allocate memory for 'data[FLAGS].data'.\n",0,0,0)
+    #endif
+    return 0;
+  }
+  
   // MACHINE_INFO array
   data[MACHINE_INFO].capacity = NUMBER_OF_INFO_ELEMENTS;
   data[MACHINE_INFO].data = load_machine_info();
 
+  if(data[MACHINE_INFO].data == NULL)
+  {
+    #ifdef TESTING_CLI
+      PRINT("Couldn't allocate memory for 'data[MACHINE_INFO].data'.\n",0,0,0)
+    #endif
+    return 0;
+  }
+  
   load_machine_info();
 
   // PERFORMANCE_INFO array
   data[PERFORMANCE_INFO].capacity = NUMBER_OF_PROGRAMS;
   data[PERFORMANCE_INFO].data = load_performance_info();
 
+  if(data[PERFORMANCE_INFO].data == NULL)
+  {
+    #ifdef TESTING_CLI
+    PRINT("Couldn't allocate memory for 'data[PERFORMANCE_INFO].data'.\n",0,0,0)
+    #endif
+    return 0;
+  }
+  
   load_performance_info();
 
   DATA_TYPE loop = NUMBER_OF_DEFAULT_ARRAYS;
@@ -40,18 +88,42 @@ void allocate_memory()
      data[loop].capacity = DEFAULT_SIZE;
      data[loop].data = (DATA_TYPE*) mmap(NULL, data[loop].capacity * sizeof(DATA_TYPE), PROT_READ|PROT_WRITE, MAP_ANONYMOUS, -1 ,0);
 
+     if(data[loop].data == NULL)
+     {
+        #ifdef TESTING_CLI
+          PRINT("Couldn't allocate memory for 'data[%u].data'",loop,0,0)
+        #endif
+        return 0;
+     }
+     
      loop++;
   }
 
   //PROGRAM
   program = (DATA_TYPE**) mmap(NULL, program_capacity * sizeof(DATA_TYPE*), PROT_READ|PROT_WRITE, MAP_ANONYMOUS, -1 ,0);
 
+  if(program == NULL)
+  {
+    #ifdef TESTING_CLI
+      PRINT("Coulnd't allocate memory for 'program'.\n",0,0,0)
+    #endif
+    return 0;
+  }
+  
   loop = 0;
 
   while(loop < program_capacity)
   {
      program[loop] = (DATA_TYPE*) mmap(NULL, PROGRAM_CHUNK_SIZE * sizeof(DATA_TYPE), PROT_READ|PROT_WRITE, MAP_ANONYMOUS, -1 ,0);
 
+     if(program[loop] == NULL)
+     {
+       #ifdef TESTING_CLI
+         PRINT("Couldn't allocate memory for 'program[%u]',\n",loop,0,0)
+       #endif
+       return 0;
+     }
+     
      loop++;
   }
 }
