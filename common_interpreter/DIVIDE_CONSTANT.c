fe5c3408b5559353d2cabd3e1881c1848e65b174
CptClutch <dria7226@colorado.edu>
2017-02-02 13:59:19 -0700

    Implemented DIVIDE_CONSTANT and GET_ARRAY_CAPACITY

--- common_interpreter/DIVIDE_CONSTANT.c
+++ common_interpreter/DIVIDE_CONSTANT.c
@@ -1,16 +1,4 @@
 // DIVIDE_CONSTANT , write_to_address , 0xdeadbeef
-if(AT_HEAD_OFFSET(1) >= CAPACITY_AT(DESTINATION_AT(head_index)))
-{
-  SET_FLAG(DATA_ACCESS_FAILED,1)
-
-  #ifdef TESTING_CLI
-    PRINT("DIVIDE_CONSTANT: DATA_ACCESS_FAILED: invalid destination address %u\n",AT_HEAD_OFFSET(1),0,0)
-  #endif
-
-  HEAD_AT(head_index) += 3;
-  goto next_instruction;
-}
-
 if(AT_HEAD_OFFSET(2) == 0)
 {
   SET_FLAG(DIVISION_BY_ZERO,1)
@@ -23,10 +11,43 @@ if(AT_HEAD_OFFSET(2) == 0)
   goto next_instruction;
 }
 
-DATA_AT(DESTINATION_AT(head_index), AT_HEAD_OFFSET(1)) /= AT_HEAD_OFFSET(2);
+if(WRITE_TO_VALUE_AT(head_index))
+{
+  if(AT_HEAD_OFFSET(1) >= program_capacity * PROGRAM_CHUNK_SIZE)
+  {
+    SET_FLAG(PROGRAM_ACCESS_FAILED,1)
+
+    #ifdef TESTING_CLI
+    PRINT("DIVIDE_CONSTANT: PROGRAM_ACCESS_FAILED: Invalid address: %u\n",AT_HEAD_OFFSET(1),0,0)
+    #endif
+
+    HEAD_AT(head_index) += 3;
+    goto next_instruction;
+  }
+
+  a = &(PROGRAM_AT(AT_HEAD_OFFSET(1)));
+}
+else
+{
+  if(AT_HEAD_OFFSET(1) >= CAPACITY_AT(DESTINATION_AT(head_index)))
+  {
+    SET_FLAG(DATA_ACCESS_FAILED,1)
+
+    #ifdef TESTING_CLI
+    PRINT("DIVIDE_CONSTANT: DATA_ACCESS_FAILED: invalid destination address %u\n",AT_HEAD_OFFSET(1),0,0)
+    #endif
+
+    HEAD_AT(head_index) += 3;
+    goto next_instruction;
+  }
+
+  a = &(DATA_AT(DESTINATION_AT(head_index), AT_HEAD_OFFSET(1)));
+}
+
+*a /= AT_HEAD_OFFSET(2);
 
 #ifdef TESTING_CLI
-  PRINT("DIVIDE_CONSTANT, %u, %u = %u\n", AT_HEAD_OFFSET(1), AT_HEAD_OFFSET(2), DATA_AT(DESTINATION_AT(head_index), AT_HEAD_OFFSET(1)) )
+  PRINT("DIVIDE_CONSTANT, %u, %u = %u\n", AT_HEAD_OFFSET(1), AT_HEAD_OFFSET(2), *a)
 #endif
 
 // advance head
--- common_interpreter/GET_ARRAY_CAPACITY.c
+++ common_interpreter/GET_ARRAY_CAPACITY.c
@@ -1,33 +1,53 @@
 // GET_ARRAY_CAPACITY, array, write_to_address
-if(AT_HEAD_OFFSET(2) >= CAPACITY_AT(DESTINATION_AT(head_index)))
-{
-  SET_FLAG(DATA_ACCESS_FAILED,2)
-
-  #ifdef TESTING_CLI
-    PRINT("GET_ARRAY_CAPACITY: DATA_ACCESS_FAILED: invalid destination address %u\n",AT_HEAD_OFFSET(2),0,0)
-  #endif
-
-  HEAD_AT(head_index) += 3;
-  goto next_instruction;
-}
-
 if(AT_HEAD_OFFSET(1) >= data_capacity)
 {
   SET_FLAG(DATA_ACCESS_FAILED,3)
 
   #ifdef TESTING_CLI
-    PRINT("GET_ARRAY_CAPACITY: DATA_ACCESS_FAILED: invalid array address %u\n",AT_HEAD_OFFSET(1),0,0)
+  PRINT("GET_ARRAY_CAPACITY: DATA_ACCESS_FAILED: invalid array address %u\n",AT_HEAD_OFFSET(1),0,0)
   #endif
 
   HEAD_AT(head_index) += 3;
   goto next_instruction;
 }
 
+if(WRITE_TO_VALUE_AT(head_index))
+{
+  if(AT_HEAD_OFFSET(2) >= program_capacity * PROGRAM_CHUNK_SIZE)
+  {
+    SET_FLAG(PROGRAM_ACCESS_FAILED,2)
 
-DATA_AT(DESTINATION_AT(head_index), AT_HEAD_OFFSET(2)) = CAPACITY_AT(AT_HEAD_OFFSET(1));
+    #ifdef TESTING_CLI
+    PRINT("GET_ARRAY_CAPACITY: Program_ACCESS_FAILED: Invalid address: %u\n",AT_HEAD_OFFSET(2),0,0)
+    #endif
+
+    HEAD_AT(head_index) += 3;
+    goto next_instruction;
+  }
+
+  a = &(PROGRAM_AT(AT_HEAD_OFFSET(2)));
+}
+else
+{
+  if(AT_HEAD_OFFSET(2) >= CAPACITY_AT(DESTINATION_AT(head_index)))
+  {
+    SET_FLAG(DATA_ACCESS_FAILED,2)
+
+    #ifdef TESTING_CLI
+    PRINT("GET_ARRAY_CAPACITY: DATA_ACCESS_FAILED: invalid destination address %u\n",AT_HEAD_OFFSET(2),0,0)
+    #endif
+
+    HEAD_AT(head_index) += 3;
+    goto next_instruction;
+  }
+
+  a = &(DATA_AT(DESTINATION_AT(head_index), AT_HEAD_OFFSET(2)));
+}
+
+*a = CAPACITY_AT(AT_HEAD_OFFSET(1));
 
 #ifdef TESTING_CLI
-  PRINT("GET_ARRAY_CAPACITY, %u, %u\n",AT_HEAD_OFFSET(1),AT_HEAD_OFFSET(2),0)
+PRINT("GET_ARRAY_CAPACITY, %u, %u\n",AT_HEAD_OFFSET(1),AT_HEAD_OFFSET(2),0)
 #endif
 
 // advance head
