253ebc67c31b1e709982a290c664b10b552b139c
root <dria7226@gmail.com>
2017-02-13 00:37:39 +0000

    Added to test_program

--- common_interpreter/4_2_head_management.c
+++ common_interpreter/4_2_head_management.c
@@ -1,7 +1,7 @@
 #ifdef TESTING_CLI
-  if(granularity_counter == 0) PRINT("HEAD: %u\n",head_index,0,0)
+if(granularity_counter == 0) PRINT("HEAD: %u\n",head_index,0,0)
 #endif
 
-head_index += (++granularity_counter == HEAD_GRANULARITY_AT(head_index));
+head_index += (++granularity_counter >= HEAD_GRANULARITY_AT(head_index));
 head_index *= ((CAPACITY_AT(HEADS) / NUMBER_OF_HEAD_COMPONENTS) > head_index);
 granularity_counter *= (HEAD_GRANULARITY_AT(head_index) > granularity_counter);
