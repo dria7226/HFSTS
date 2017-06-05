49b19f1302409e842275455b3f424b525a12546c
root <dria7226@gmail.com>
2017-06-05 01:16:09 +0000

    First round of descriptions added for the wiki.

--- update_wiki.sh
+++ update_wiki.sh
@@ -4,28 +4,23 @@ cd ./HFSTS.wiki_cpp
 
 output_path="core_programs.txt"
 
-cpp -P -D ENUMERATE -D NAME_MODE ../common_interpreter/core_programs.c > $output_path
+cpp -P -D NAME_MODE ../common_interpreter/core_programs.c > $output_path
 
 program_code=0
-IFS=','
+
 while read line
 do
     cp core_program_template.txt ../HFSTS.wiki/$line.txt
     sed -i 's/FILENAME/'$line'/g' ../HFSTS.wiki/$line.txt
     sed -i 's/program_code_code/'$program_code'/g' ../HFSTS.wiki/$line.txt
     let program_code=program_code+1
+    cpp ../HFSTS.wiki/$line.txt -P ../HFSTS.wiki/$line.md
+    rm ../HFSTS.wiki/$line.txt
 done < $output_path
 
 rm $output_path
 
 cd ../HFSTS.wiki
-
-# preprocess generated operation files
-
-for filename in *.txt
-do
-    cpp $filename -P $(basename "$filename" .txt).md
-done
 
 #push wiki to git
 git add .
